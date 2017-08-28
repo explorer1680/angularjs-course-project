e994f05d5d0ed2b3a9cf18367e514f85bbdc389a
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-08-28 13:58:33 -0400

    add content to the project.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../Recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+      new Recipe('A Test Recipe', 'This is a simply a test', 'https://media.treehugger.com/assets/images/2013/06/JJH_3362-1.jpg.650x0_q70_crop-smart.jpg'),
+      new Recipe('A Test Recipe', 'This is a simply a test', 'https://media.treehugger.com/assets/images/2013/06/JJH_3362-1.jpg.650x0_q70_crop-smart.jpg')
+    ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
