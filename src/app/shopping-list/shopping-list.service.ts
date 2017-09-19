646bd9331b243a0a3eb2e9f2aec14d2a88a72bf9
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-09-19 16:57:30 -0400

    add ingredient from recipe page.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,12 @@ export class ShoppingListService {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
+
+    addIngredients(ingredients: Ingredient[]){
+        // for(let ingredient of ingredients){
+        //     this.addIngredient(ingredient); //this will emit a lot of events.
+        // }
+        this.ingredients.push(...ingredients); //EX6 feature: spread operator, turn array of element to list of element.
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
