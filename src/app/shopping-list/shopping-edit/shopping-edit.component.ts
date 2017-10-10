2012c12dd5a9092cceebd1b554c6d9abbd060c36
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-10 16:03:18 -0400

    add function on Clear button.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -60,4 +60,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
     form.reset();
   }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
