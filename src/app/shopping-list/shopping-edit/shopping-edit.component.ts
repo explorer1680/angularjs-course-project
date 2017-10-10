3301202f713c09f1e3a0c1cf023b23aa8356bdc9
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-10 15:57:21 -0400

    clean the form after submit.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -46,7 +46,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     // const ingredientName = this.nameInputRef.nativeElement.value;
     // const ingredientAmount = this.amountInput.nativeElement.value;
     const value = form.value;
@@ -57,5 +57,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 }
