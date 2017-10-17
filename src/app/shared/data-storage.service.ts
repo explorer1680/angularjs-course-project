8da83908e9389b6bc0087c2be95e14dd756cbed3
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-17 16:43:47 -0400

    implement auth with save and get data from firebase.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,23 +4,27 @@ import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
-        return this.http.put('https://ng-recipe-book-a8294.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-a8294.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-a8294.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+
+        this.http.get('https://ng-recipe-book-a8294.firebaseio.com/recipes.json?auth=' + token)
             .map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
-                for(let recipe of recipes) {
-                    if(!recipe['ingredients']){
+                for (let recipe of recipes) {
+                    if (!recipe['ingredients']) {
                         recipe['ingredients'] = [];
                     }
                 }
