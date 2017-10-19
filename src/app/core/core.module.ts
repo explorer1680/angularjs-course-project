c4b6d80b0444b2f0063aac49a3e7774af705157b
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-19 15:57:29 -0400

    move service to core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthGuard } from '../auth/auth-guard.service';
+
 
 @NgModule({
     declarations: [
@@ -18,6 +24,9 @@ import { SharedModule } from '../shared/shared.module';
     exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService, RecipeService, DataStorageService, AuthService
     ]
 })
 export class CoreModule{}
