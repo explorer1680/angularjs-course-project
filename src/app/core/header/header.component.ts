9bc9e78605466075ec1764e77f14ea8f2541174a
echeng <cjs1234567@hotmail.com>
2017-10-19 16:42:17 -0400

    modify code, try to pass ng build.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
 import { Response } from '@angular/http';
 import { AuthService } from '../../auth/auth.service';
@@ -8,7 +8,9 @@ import { AuthService } from '../../auth/auth.service';
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    authService2 : AuthService;
 
     constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
 
@@ -26,5 +28,9 @@ export class HeaderComponent {
 
     onLogout(){
         this.authService.logout();
+    }
+
+    ngOnInit(){
+        this.authService2 = this.authService;
     }
 }
