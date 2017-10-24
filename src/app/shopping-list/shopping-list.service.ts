9526f4fc26e913fa4c00363705f521e65b3aaed0
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-24 15:54:12 -0400

    add comment.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,44 +1,46 @@
-// import { EventEmitter } from '@angular/core';
-// import { Ingredient } from '../shared/ingredient.model';
-// import { Subject } from 'rxjs/Subject';
+// this file in not need any more.
 
-// export class ShoppingListService {
-//     // ingredientsChanged = new EventEmitter<Ingredient[]>();
-//     ingredientsChanged = new Subject<Ingredient[]>();
-//     startedEditing = new Subject<number>();
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject';
 
-//     private ingredients: Ingredient[] = [
-//         new Ingredient('Apples', 5),
-//         new Ingredient('Tomatoes', 10)
-//     ];
+export class ShoppingListService {
+    // ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
-//     // getIngredients() {
-//     //     return this.ingredients.slice();
-//     // }
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
 
-//     getIngredient(index: number){
-//         return this.ingredients[index];
-//     }
-//     // addIngredient(ingredient: Ingredient) {
-//     //     this.ingredients.push(ingredient);
-//     //     this.ingredientsChanged.next(this.ingredients.slice());
-//     // }
+    // getIngredients() {
+    //     return this.ingredients.slice();
+    // }
 
-//     addIngredients(ingredients: Ingredient[]){
-//         // for(let ingredient of ingredients){
-//         //     this.addIngredient(ingredient); //this will emit a lot of events.
-//         // }
-//         this.ingredients.push(...ingredients); //EX6 feature: spread operator, turn array of element to list of element.
-//         this.ingredientsChanged.next(this.ingredients.slice());
-//     }
+    getIngredient(index: number){
+        return this.ingredients[index];
+    }
+    // addIngredient(ingredient: Ingredient) {
+    //     this.ingredients.push(ingredient);
+    //     this.ingredientsChanged.next(this.ingredients.slice());
+    // }
 
-//     updateIngredient(index: number, newIngredient: Ingredient){
-//         this.ingredients[index] = newIngredient;
-//         this.ingredientsChanged.next(this.ingredients.slice());
-//     }
+    addIngredients(ingredients: Ingredient[]){
+        // for(let ingredient of ingredients){
+        //     this.addIngredient(ingredient); //this will emit a lot of events.
+        // }
+        this.ingredients.push(...ingredients); //EX6 feature: spread operator, turn array of element to list of element.
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 
-//     deleteIngredient(index: number){
-//         this.ingredients.splice(index, 1);
-//         this.ingredientsChanged.next(this.ingredients.slice());
-//     }
-// }
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number){
+        this.ingredients.splice(index, 1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+}
