0547d29a981afb25410195cddef15d3d4345e0df
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-25 15:05:45 -0400

    fix bug.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,19 +5,32 @@ import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
+    constructor(private http: Http,
+        private recipeService: RecipeService,
+        // private authService: AuthService
+        private store: Store<fromApp.AppState>) { }
 
     storeRecipes() {
-        const token = this.authService.getToken();
+        // const token = this.authService.getToken();
+        const token = this.store.select('auth').map((authState: fromAuth.State) => {
+            return authState.authenticated;
+        });
+
         return this.http.put('https://ng-recipe-book-a8294.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        const token = this.authService.getToken();
+        // const token = this.authService.getToken();
+        const token = this.store.select('auth').map((authState: fromAuth.State) => {
+            return authState.authenticated;
+        });
 
         this.http.get('https://ng-recipe-book-a8294.firebaseio.com/recipes.json?auth=' + token)
             .map(
