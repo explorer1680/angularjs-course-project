0c58812809bce111367ff0543dae14e8d618d505
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-10-25 14:49:23 -0400

    finish auth part using ngrx/store.

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,17 +1,27 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanLoad, Route } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './store/auth.reducers';
 
 @Injectable()
-export class AuthGuard implements CanActivate, CanLoad{
+export class AuthGuard implements CanActivate, CanLoad {
 
-    constructor(private authService: AuthService) {}
+    // constructor(private authService: AuthService) {}
+    constructor(private store: Store<fromApp.AppState>) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.authService.isAuthenticated();
+        // return this.authService.isAuthenticated();
+        return this.store.select('auth').map((authState: fromAuth.State) => {
+            return authState.authenticated;
+        });
     }
 
-    canLoad(route:Route){
-        return this.authService.isAuthenticated();
+    canLoad(route: Route) {
+        // return this.authService.isAuthenticated();
+        return this.store.select('auth').map((authState: fromAuth.State) => {
+            return authState.authenticated;
+        });
     }
 }
